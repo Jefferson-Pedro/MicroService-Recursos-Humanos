9db03a0e9d2642bcae570e673566ee61372681b6
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-20 19:45:27 -0300

    Update PaymentService.java
    - Configuração da classe de comunicação entre um projeto e outro, através de um método;

--- hr-payroll/src/main/java/br/com/jeffersonpedro/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/jeffersonpedro/payroll/services/PaymentService.java
@@ -1,14 +1,31 @@
 package br.com.jeffersonpedro.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.jeffersonpedro.payroll.entites.Payment;
+import br.com.jeffersonpedro.payroll.entites.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
