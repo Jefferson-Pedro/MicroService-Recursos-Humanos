d2b0d36c4560e5e136be93677634576506759066
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-19 22:20:34 -0300

    Update WorkerRepository.java

--- hr-worker/src/main/java/br/com/jeffersonpedro/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/br/com/jeffersonpedro/hrworker/repositories/WorkerRepository.java
@@ -1,5 +1,9 @@
 package br.com.jeffersonpedro.hrworker.repositories;
 
-public class WorkerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.jeffersonpedro.hrworker.entites.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
 
 }
