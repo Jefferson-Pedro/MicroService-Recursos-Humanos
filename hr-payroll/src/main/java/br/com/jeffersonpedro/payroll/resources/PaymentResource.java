b337277146a563a59a96e6001cc0dbc88345abfa
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-20 21:51:50 -0300

    Update PaymentResource.java
    - Refatoração da classe responsavel pela implementação configuração do método responsavel pela comunicação entre os dois projetos;

--- hr-payroll/src/main/java/br/com/jeffersonpedro/payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/jeffersonpedro/payroll/resources/PaymentResource.java
@@ -19,7 +19,7 @@ public class PaymentResource {
 	
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
-		System.out.println(workerId + days);
+		
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
