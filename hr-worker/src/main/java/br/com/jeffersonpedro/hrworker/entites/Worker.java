0ebae80cbcd9f646b212509ce5404b0f87b2c2ee
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-03-23 14:04:30 -0300

    - Tentativa de Correção do projeto em Micro Serviços
    - O projeto foi construido com uma versão diferente do Spring, o que quebrou algumas bibliotecas;

--- hr-worker/src/main/java/br/com/jeffersonpedro/hrworker/entites/Worker.java
+++ hr-worker/src/main/java/br/com/jeffersonpedro/hrworker/entites/Worker.java
@@ -3,11 +3,11 @@ package br.com.jeffersonpedro.hrworker.entites;
 import java.io.Serializable;
 import java.util.Objects;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
