762a5ae06d3860d0888f7d61f13ecbe48655cb59
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-09-09 19:11:54 -0300

    Rest Template

--- hr-payroll/src/main/java/br/com/hr_payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/hr_payroll/resources/PaymentResource.java
@@ -18,7 +18,7 @@ public class PaymentResource {
 	private PaymentService service;
 	
 	@GetMapping("/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Double workerId, @PathVariable Integer days){
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
