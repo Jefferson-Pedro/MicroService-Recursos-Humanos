4c45548b9b4b4962145265302c72ba6aee73378a
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-09-09 18:01:33 -0300

    create project hr-payroll

---
+++ hr-payroll/src/main/java/br/com/hr_payroll/HrHrPayrollApplication.java
@@ -0,0 +1,13 @@
+package br.com.hr_payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrHrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrHrPayrollApplication.class, args);
+	}
+
+}
