a7155c1dbd13b1355f29a5334e5e34bb4ebd422f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-09-10 18:57:12 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/br/com/hr_gateway/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hr_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
