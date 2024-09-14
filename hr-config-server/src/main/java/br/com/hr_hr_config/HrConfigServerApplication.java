1f4167d5fdcefa2b2b3feba0284f296914752432
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-09-14 11:12:17 -0300

    Private Git Repository

--- hr-config-server/src/main/java/br/com/hr_hr_config/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/hr_hr_config/HrConfigServerApplication.java
@@ -1,5 +1,7 @@
 package br.com.hr_hr_config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,9 +9,21 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
+	
+	/*
+	 * @Value("${spring.cloud.config.server.git.username}") String username;
+	 * 
+	 * @Value("${spring.cloud.config.server.git.password}") String pass;
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
+
+	/*
+	 * @Override public void run(String... args) throws Exception {
+	 * System.out.println("USERNAME = " + username); System.out.println("PASS = " +
+	 * pass); }
+	 */
 
 }
