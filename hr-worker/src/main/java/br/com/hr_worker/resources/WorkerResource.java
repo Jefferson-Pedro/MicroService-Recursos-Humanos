0d791c57b6f0592c860f229b061c47716173d466
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-09-15 22:28:04 -0300

    Zuul Authorization

--- hr-worker/src/main/java/br/com/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/hr_worker/resources/WorkerResource.java
@@ -39,7 +39,7 @@ public class WorkerResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	@GetMapping
+	@GetMapping("/all")
 	public ResponseEntity<List<Worker>> findAll(){
 		
 		List<Worker> list = repository.findAll();
@@ -50,14 +50,13 @@ public class WorkerResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		
+		/*
 		try { 
 			 Thread.sleep(3000L); 
 		} catch (InterruptedException e) { 
 			e.printStackTrace(); 
-		}
-		 
-		
+		}*/
+		 		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		Worker obj = repository.findById(id).get();
