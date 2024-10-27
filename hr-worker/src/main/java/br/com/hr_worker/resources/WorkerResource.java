e9c3d57f46ee94b84adb5b4223534ad9f918132f
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-10-27 18:44:04 -0300

    Update WorkerResource.java
    - Create register new worker;

--- hr-worker/src/main/java/br/com/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/hr_worker/resources/WorkerResource.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,6 +63,14 @@ public class WorkerResource {
 		Worker obj = repository.findById(id).get();
 				
 		return ResponseEntity.ok(obj);
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<Worker> register(@RequestBody Worker worker) {
+		
+	    Worker obj = repository.save(worker);
+	    
+	    return ResponseEntity.ok(obj);
 	}
 
 }
